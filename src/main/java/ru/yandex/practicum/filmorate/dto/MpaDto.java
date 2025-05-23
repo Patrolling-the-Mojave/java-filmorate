e6b6455d0a1ed2e1c14c76632dc61dc2cc1bb564
Соловьенко Никита <nikidan6032638@yandex.ru>
2025-05-23 03:11:52 +0300

    добавлена логика работы с бд

--- src/main/java/ru/yandex/practicum/filmorate/dto/MpaDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/MpaDto.java
@@ -1,15 +1,13 @@
 package ru.yandex.practicum.filmorate.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class MpaDto {
-    private Integer id;
+    private final Integer id;
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String name;
 }
